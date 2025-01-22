2817008c016b316b626cc939e169f300b001754f
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-21 20:29:51 -0600

    Creating student routes

---
+++ data/assistants.js
@@ -0,0 +1,24 @@
+// Assistants database
+
+const assistantsDB = [
+    {
+        id: 1,
+        name: "Mike",
+        phone: "12346589",
+        email: "mike@SBA.PS"
+    },
+    {
+        id: 2,
+        name: "John",
+        phone: "12346589",
+        email: "john@SBA.PS"        
+    },
+    {
+        id: 3,
+        name: "Lisa",
+        phone: "12346589",
+        email: "lisa@SBA.PS"        
+    }
+]
+
+module.exports = assistantsDB;
--- server.js
+++ server.js
@@ -31,6 +31,36 @@ app.get ('/', (req, res) => {
         `);
 });
 
+
+//Students Routes
+app.route("/students")
+    // GET the Students Database
+    .get((req,res) => {
+        res.json(students)
+    })
+    // POST in the Students Database
+    .post((req,res) => {
+        
+        if (req.body.name && req.body.phone && req.body.email) {
+            // Checking for duplicates
+            if (students.find((student) => student.name == req.body.name)) {
+                res.send(`<h3 style="color:darkred">Student already exist</h3>`);
+                return;
+            }
+            // If not exist creating a new object to the student
+            const student = {
+                id: students[students.length - 1].id + 1, // New Student ID
+                name: req.body.name, // New student name
+                phone: req.body.phone, // New student phone number
+                email: req.body.email // New student email address
+            };
+            students.push(student); // Adding the new student object to the students array.
+            res.json(students[students.length - 1]); // Returning a JSON response with the new student information.
+        }
+        else res.send(`<h3 style="color:darred">ERROR: Incorrect Data</h3>`);
+    })
+
+
 //Students Routes
 app.get("/students", (req,res) => {
     res.json(students);
