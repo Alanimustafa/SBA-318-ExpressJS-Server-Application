12e07ab34cfcce4e77efd2daf0d25f1fca42e7ab
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-21 18:17:46 -0600

    Home route

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Variables Declaration
+
 // Requiring Express
 const express = require ('express');
 
@@ -10,7 +12,35 @@ const PORT = 3000;
 // Parsing the data
 const bodyParser = require("body-parser");
 
+// Importing data from databases
+const students = require ('./data/students');
+const instructors = require ('./data/instructors');
+const assistants = require ('./data/assistants');
 
+
+//--------------- Routes ------------------------------
+
+// Home Page
+app.get ('/', (req, res) => {
+    res.send(`
+        <h1>Class Contact</h1>
+        `)
+})
+
+//------------------ Middleware --------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------- Starting the Server --------------------
 // Starting the Server to PORT 3000
 app.listen(PORT, (req, res) => {
     console.log(`Server APP started at port# ${PORT}`);
